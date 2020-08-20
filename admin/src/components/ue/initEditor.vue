86a1dbfc874427e4846c0832d14b0c0901d03d2e
xuyangyang2019 <15268296911@163.com>
2020-08-20 16:17:55 +0800

    format

--- admin/src/components/ue/initEditor.vue
+++ admin/src/components/ue/initEditor.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-    import { mapActions } from "vuex" 
+    import { mapActions } from "vuex"
     import UE from './UEditor'
     export default {
         components:{
